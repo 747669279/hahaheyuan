21259df514667bcdabac4ae85959837be5f076d9
747669279 <hejun@hejun.local>
2016-06-26 23:47:29 +0800

    +
    +

---
+++ XinMinClub/XinMinClub/Home/Library/View/DiagramsView2.h
@@ -0,0 +1,13 @@
+//
+//  DiagramsView2.h
+//  XinMinClub
+//
+//  Created by 贺军 on 16/6/26.
+//  Copyright © 2016年 yangkejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiagramsView2 : UIView
+
+@end
