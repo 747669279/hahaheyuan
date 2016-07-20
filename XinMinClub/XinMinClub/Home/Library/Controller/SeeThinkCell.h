96af22f00ae72a23780f4e798313859bd321639a
YangKJ <393103982@qq.com>
2016-07-20 16:55:00 +0800

    Revert "Revert "7-20 16：18""
    This reverts commit e38a4fa398672682166fd5cc555953c217a9ad90.

---
+++ XinMinClub/XinMinClub/Home/Library/Controller/SeeThinkCell.h
@@ -0,0 +1,13 @@
+//
+//  SeeThinkCell.h
+//  XinMinClub
+//
+//  Created by 赵劲松 on 16/7/19.
+//  Copyright © 2016年 yangkejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeeThinkCell : UITableViewCell
+
+@end
